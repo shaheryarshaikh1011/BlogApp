6457d5e3e0566795d92c8bae90cdcccc0345752e
Shaheryar Shaikh <43124039+avc52github@users.noreply.github.com>
2020-08-31 21:45:50 +0530

    version 1 adding files

---
+++ app.js
@@ -0,0 +1,136 @@
+var express= 		require("express");
+	bodyParser=		require("body-parser");
+	methodOverride=	require("method-override");
+	mongoose=		require("mongoose");
+	app=			express();
+//app config
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+//Mongoose/model config
+var BlogSchema= new mongoose.Schema({
+	title: String,
+	image:String,
+	body:String,
+	created:{type:Date,default:Date.now}
+});
+
+var Blog=mongoose.model("Blog",BlogSchema);
+
+ //  Blog.create({
+ //  	title:"Test Blog",
+ //  	image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+ //  	body:"Hello this is a blogpost"	
+ // });
+
+
+//ROutes
+
+
+//Redirecting to home
+app.get("/",function(req,res) {
+	res.redirect("/blogs");
+	// body...
+})
+
+
+//Index route
+app.get("/blogs",function(req,res) {
+	Blog.find({},function(err,blogs) {
+		if(err)
+		{
+			console.log("error");
+		}
+		else
+		{
+			res.render("index",{blogs:blogs});
+		}
+		// body...
+	});
+
+	// body...
+});
+
+
+
+//New route
+app.get("/blogs/new",function(req,res) {
+	res.render("new");
+	// body...
+});
+
+//CREAte router
+app.post("/blogs",function(req,res) {
+	//create blog
+	Blog.create(req.body.blog,function(err,newBlog) {
+		if(err)
+		{
+			console.log(err);
+			res.render("new");
+		}
+		else
+		{
+			//then redirect to index
+			res.redirect("/blogs");	
+		}
+		// body...
+	})
+	
+	});
+
+
+//SHOW route
+app.get("/blogs/:id",function(req,res) {
+	Blog.findById(req.params.id,function(err,foundBlog) {
+		if(err)
+		{
+			res.redirect("/blogs");
+		}
+		else
+		{
+			res.render("show",{blog:foundBlog});
+		}
+		// body...
+	});
+	// body...
+});
+
+
+//Edit route
+app.get("/blogs/:id/edit",function(req,res) {
+	Blog.findById(req.params.id,function(err,foundBlog)
+	{
+		if(err)
+		{
+			res.redirect("/blogs");
+		}
+		else
+		{
+			res.render("edit",{blog:foundBlog});
+		}
+	});
+	
+	// body...
+});
+
+//UPdate route
+app.put("/blogs/:id",function(req,res) {
+	res.send("update done");
+	// body...
+})
+// title 
+// image
+// body
+// created
+
+
+
+app.listen(3000,'localhost',function() {
+	// body...
+	console.log("Listening to port"+3000);
+	console.log("BlogApp Local Server has Started");
+
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+##intro
+
+
+#blog index
+setup blog app
+create blog model
+add index route and template
+
+#basic layout
+add header and footer partials
+include semantic ui
+add simple nav
